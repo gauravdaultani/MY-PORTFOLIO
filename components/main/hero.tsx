bec7db0809fbd9e721c8b95d3321c4b46f1914c8
gauravdaulatni <gauravdaultani8@gmail.com>
2024-05-11 13:30:13 +0530

    Update title and description in layout.tsx

---
+++ components/main/hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface Props {}
+
+const Hero = () => {
+  return (
+  <div className= "flex flex.col w-full h-full">
+    <video
+    autoPlay
+    loop
+    muted
+    className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover"
+    >
+      <source src="/blackhole.webm" type="video/webm"/> 
+    </video>
+  </div>
+  )
+
+}
+
+export default Hero
