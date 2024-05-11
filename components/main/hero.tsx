b9ba6ee31d615c2b07ea9b90e4b54743afa4896a
gauravdaulatni <gauravdaultani8@gmail.com>
2024-05-11 16:53:26 +0530

    Add new font and styles to globals.css, update hero.tsx and package.json

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import Herocmponent from './sub/Herocmponent'
 
 interface Props {}
 
 const Hero = () => {
   return (
-  <div className= "flex flex.col w-full h-full">
+  <div className= "relative flex flex.col w-full h-full">
     <video
     autoPlay
     loop
     muted
     className="rotate-180 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover"
     >
-      <source src="/blackhole.webm" type="video/webm"/> 
+      <source src="/blackhole.webm" type="video/webm" /> 
     </video>
+    <Herocmponent />
   </div>
   )
 
